61690bdc34aebd2d0b0c2c22f2d33e7ce648965d
Karina Kristensen <karinakristensen2@gmail.com>
2024-11-12 14:46:35 +0100

    Add project files.

---
+++ Client/Models/EntryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppClientServer.Client.Models
+{
+    public class EntryModel
+    {
+        public string Name { get; set; }
+        public string SelectedYdelse { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; } = DateTime.Today;
+    }
+}
