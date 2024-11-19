aefc742a1a1e823c212d3cee6fc52dfbedebbde7
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2024-11-19 09:06:29 +0100

    Added the nullables clause to the database

--- Shared/Models/Ordre.cs
+++ Shared/Models/Ordre.cs
@@ -7,13 +7,13 @@
 		public bool Status { get; set; }
 
 		public int? YdelseListeId { get; set; }
-		public YdelseListe YdelseListe { get; set; }
+		public YdelseListe? YdelseListe { get; set; }
 
 		public int? KundeId { get; set; }
-		public Kunde Kunde { get; set; }
+		public Kunde? Kunde { get; set; }
 
 		public int? MekanikerId { get; set; }
-		public Mekaniker Mekaniker { get; set; }
+		public Mekaniker? Mekaniker { get; set; }
 
 		public override string ToString()
 		{
