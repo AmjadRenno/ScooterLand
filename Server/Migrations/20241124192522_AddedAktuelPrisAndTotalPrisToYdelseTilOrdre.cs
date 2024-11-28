0083ebc9d6410435c425f5e3e329fcf829fe62a8
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2024-11-28 11:17:19 +0100

    Ændrede Migrations

--- Server/Migrations/20241124192522_AddedAktuelPrisAndTotalPrisToYdelseTilOrdre.cs
+++ Server/Migrations/20241124192522_AddedAktuelPrisAndTotalPrisToYdelseTilOrdre.cs
@@ -1,6 +1,50 @@
-﻿namespace BlazorAppClientServer.Server.Migrations
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorAppClientServer.Server.Migrations
 {
-	public class _20241124192522_AddedAktuelPrisAndTotalPrisToYdelseTilOrdre
+	/// <inheritdoc />
+	public partial class AddedAktuelPrisAndTotalPrisToYdelseTilOrdrecs : Migration
 	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.RenameColumn(
+				name: "YdelseMængdeId",
+				table: "YdelseMængder",
+				newName: "YdelseTilOrdreId");
+
+			migrationBuilder.AddColumn<double>(
+				name: "AktuelPris",
+				table: "YdelseMængder",
+				type: "float",
+				nullable: false,
+				defaultValue: 0.0);
+
+			migrationBuilder.AddColumn<double>(
+				name: "TotalPris",
+				table: "YdelseMængder",
+				type: "float",
+				nullable: false,
+				defaultValue: 0.0);
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				name: "AktuelPris",
+				table: "YdelseMængder");
+
+			migrationBuilder.DropColumn(
+				name: "TotalPris",
+				table: "YdelseMængder");
+
+			migrationBuilder.RenameColumn(
+				name: "YdelseTilOrdreId",
+				table: "YdelseMængder",
+				newName: "YdelseMængdeId");
+		}
 	}
 }
