387efc018afba65fac79578febcc416020f800ad
René Rønning Hertz <rrhe61883@edu.ucl.dk>
2024-12-06 11:06:07 +0100

    Ændringer i mærke

--- Client/Services/MærkeService.cs
+++ Client/Services/MærkeService.cs
@@ -17,31 +17,31 @@ namespace BlazorAppClientServer.Client.Services
 
         public async Task<Mærke[]?> GetAllMærker()
         {
-			var result = await httpClient.GetFromJsonAsync<Mærke[]>("api/mærkeapi");
+			var result = await httpClient.GetFromJsonAsync<Mærke[]>("api/maerkeapi");
 			return result;
         }
 
         public async Task<Mærke> GetMærkeById(int id)
         {
-            var result = await httpClient.GetFromJsonAsync<Mærke>($"api/mærkeapi/{id}");
+            var result = await httpClient.GetFromJsonAsync<Mærke>($"api/maerkeapi/{id}");
             return result;
         }
 
         public async Task<bool> AddMærke(Mærke mærke)
         {
-            var response = await httpClient.PostAsJsonAsync("api/mærkeapi", mærke);
+            var response = await httpClient.PostAsJsonAsync("api/maerkeapi", mærke);
             return response.IsSuccessStatusCode;
         }
 
         public async Task<bool> UpdateMærke(Mærke mærke)
         {
-            var response = await httpClient.PutAsJsonAsync("api/mærkeapi", mærke);
+            var response = await httpClient.PutAsJsonAsync("api/maerkeapi", mærke);
             return response.IsSuccessStatusCode;
         }
 
         public async Task<bool> DeleteMærke(int id)
         {
-            var response = await httpClient.DeleteAsync($"api/mærkeapi/{id}");
+            var response = await httpClient.DeleteAsync($"api/maerkeapi/{id}");
             return response.IsSuccessStatusCode;
         }
     }
--- Server/Program.cs
+++ Server/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddScoped<IYdelseRepository, YdelseRepository>();
 builder.Services.AddScoped<IKundeRepository, KundeRepository>();
 builder.Services.AddScoped<IMekanikerRepository, MekanikerRepository>();
 builder.Services.AddScoped<IOrdreRepository, OrdreRepository>();
+builder.Services.AddScoped<IMærkeRepository, MærkeRepository>();
+
 builder.Services.AddControllers().AddJsonOptions(x =>
    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
