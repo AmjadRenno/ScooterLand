6d96ba82ecfaacbe4f9322ef9e091808d2e85142
ThomasWin <tomner01@gmail.com>
2024-12-10 14:50:24 +0100

    Fjernet fejlgivne kode

--- Shared/Models/Faktura.cs
+++ Shared/Models/Faktura.cs
@@ -3,7 +3,6 @@
 	public class Faktura
 	{
 		public int FakturaId {  get; set; }
-		[Required(ErrorMessage = "Ordre-ID er påkrævet")]
 		public int? OrdreId { get; set; }
 		public Ordre? Ordre { get; set; }
 	}
